855fbe7eb2254434f1af7cbbf679d0b5a2d59e4b
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2021-12-29 22:22:50 -0300

    Exercicios 1 e 2

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/Conteudo/main.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/Conteudo/main.js
@@ -14,20 +14,57 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-    
+
+function clicou(event){
+  let classeTech = document.querySelector('.tech');
+  classeTech.classList.remove('tech');
+  event.target.classList.add('tech');
+  input.value = '';
+}  
+
+firstLi.addEventListener('click',clicou);
+secondLi.addEventListener('click',clicou);
+thirdLi.addEventListener('click',clicou);
+
 
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
-    
 
+input.addEventListener('input', function(event) {
+  const techElement = document.querySelector('.tech');
+  techElement.innerText = event.target.value;
+});
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
 
+    let spotrybefy = document.getElementsByTagName('h1')[0];
+    spotrybefy.addEventListener('dblclick', redireciona);
+
+    function redireciona(teste){
+      spotrybefy.innerHTML = window.location.href="https://thiagozardo.github.io/";
+    }
+
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
+firstLi.addEventListener('mouseenter',passarMouse);
+secondLi.addEventListener('mouseenter',passarMouse);
+thirdLi.addEventListener('mouseenter',passarMouse);
+
+
+
+function passarMouse(event){
+  firstLi.style.backgroundColor='#2fc18c';
+  secondLi.style.backgroundColor='#006dfb';
+  thirdLi.style.backgroundColor='#41197f';
+  event.target.style.backgroundColor='rgb(130,130,130)';
+}
+
+
 
 // Segue abaixo um exemplo do uso de event.target:
 

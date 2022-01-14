de2eac178a6663ed252e7ef1e96aba6719e50429
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-01-14 01:58:54 -0300

    Parte02

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6/objetosParte1.js
@@ -0,0 +1,50 @@
+//Conteúdo Parte 01
+/*
+const pessoa = {
+  nome: 'Thiago',
+  idade: 35,
+  proffissao: 'programador',
+}
+
+const adcionaChave = () => {
+  pessoa.sobrenome = 'zardo';
+
+  return pessoa;
+}
+console.log(adcionaChave(pessoa));
+
+
+
+
+//Conteúdo Parte 02
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const listaHabilidades = (estudante) => {
+  const arrayDeHabilidades = Object.keys(estudante);
+  for(let i = 0; i < arrayDeHabilidades.length; i+=1){
+    console.log(`${arrayDeHabilidades[i]}, Nível: ${estudante[arrayDeHabilidades[i]]}`);
+  }
+};
+
+console.log('Estudante 1');
+listaHabilidades(student1);
+console.log('-----------------')
+
+console.log('Estudante 2');
+listaHabilidades(student2);
+*/
+
+

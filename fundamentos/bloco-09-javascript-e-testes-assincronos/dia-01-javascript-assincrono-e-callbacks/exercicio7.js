84a17e78fb86133b7f713b8d81678221c111cd67
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-02-01 11:28:42 -0300

    Ex 08

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicio7.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicio7.js
@@ -3,7 +3,14 @@ const uppercase = (str, callback) => {
     callback(str.toUpperCase());
   }, 500);
 };
-
-uppercase('texto transformado em maiusculo');
-
-module.exports = { uppercase };
+// Não consegui fazer pois não esta rodando o npm test para essa função.
+it('uppercase "test" to equal "TEST"', (done) => {
+  uppercase('test', (str) => {
+    try {
+      expect(str).toBe('TEST');
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicio7.test.js
+++
@@ -1,5 +0,0 @@
-const { uppercase } = require('exercicio7');
-
-test('verifique se transforma as letras de uma palavra em letras maiúsculas.', () => {
-  expect("texto transformado em maiusculo").toBe('TEXTO TRANSFORMADO EM MAISUCULO');
-});

6a2df9d2886a9ec40303dccef65134218ed8e12a
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-02-01 00:15:05 -0300

    Ex05

---
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/.test.js
@@ -0,0 +1,48 @@
+// Testes Assincronos com Callbacks
+/* 01
+
+test('Não deveria passar!', (done) => {
+  setTimeout(() => {
+    try {
+      expect(10).toBe(5);
+      console.log('Deveria falhar!');
+      done();
+    } catch (error) { 
+      done(error);
+    }  
+  }, 500);
+});
+
+// 02
+const asyncSum = (a, b, callback) => {
+  setTimeout(() => {
+    const result = a + b +1;
+    callback(result);
+  }, 500);
+};
+
+test('Testando asyncSum, soma 5 mais 10', (done) => {
+  asyncSum(5, 10, (result) => {
+    try {
+      expect(result).toBe(15);
+      done();
+    } catch (error) {
+      done(error);
+    }
+  }, 500);
+});
+*/
+
+
+cicles.test.js
+
+// let cities = [];
+
+// const addCity = (city) => {
+//  cities.push(city);
+// };
+
+// const removeCity = (city) => {
+//  cities = cities.filter((eachCity) => eachCity !== city);
+// };
+

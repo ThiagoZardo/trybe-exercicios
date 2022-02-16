288f5f0dd0767ba9385318d88712e844182a9914
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-02-16 17:35:59 -0300

    Primeiro conteudo ok

---
+++ frontend/bloco-10-introducao-a-react/dia-1-introducao/conteudo2/addconteudo2/src/Component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Component } from "react";
+
+class hello extends React.Component {
+  render() {
+    const name = <h1>Thiago Zardo</h1>;
+    return name;
+  }
+}
+
+export default Component;

71982913fe29b5ad1ff2689f9e77c0166546c662
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-02-16 18:39:29 -0300

    Exercicios 1 e 2

---
+++ frontend/bloco-10-introducao-a-react/dia-1-introducao/exercicios/app-exercicio/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+const arrayCompromissos = ['Preparar conteúdo do dia', 'Estudar Conteúdo, Fazer Aula Ao Vivo', 'Fazer Exercícios', 'Ir para o Fechamento'];
+
+const Task = (value) => {
+  return (
+    arrayCompromissos.map((compromisso) => <li> {compromisso}</li>)
+  );
+}
+
+
+export default Task;

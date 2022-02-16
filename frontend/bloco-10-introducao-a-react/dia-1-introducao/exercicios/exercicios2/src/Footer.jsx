5771745d9f8383712d6b6c897219f8f6bf5ee2ea
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-02-16 20:50:25 -0300

    Exercicio 3

---
+++ frontend/bloco-10-introducao-a-react/dia-1-introducao/exercicios/exercicios2/src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <h1>E isso é só o começo...</h1>
+    )
+  }
+}
+
+export default Footer;

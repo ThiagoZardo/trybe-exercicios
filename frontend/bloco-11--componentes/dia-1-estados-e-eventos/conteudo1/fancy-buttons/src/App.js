24ff130ee33773b23c03124b433bb0945dae3c2c
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-02-20 19:51:52 -0300

    Fixaçao 3 e 4

--- frontend/bloco-11--componentes/dia-1-estados-e-eventos/conteudo1/fancy-buttons/src/App.js
+++ frontend/bloco-11--componentes/dia-1-estados-e-eventos/conteudo1/fancy-buttons/src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
 
-const funClick = () => {
-  console.log('clicou');
-}
-
-const funClick2 = () => {
-  console.log('clicou2');
-}
-
-const funClick3 = () => {
-  console.log('clicou3');
-}
 
 class Fancy extends React.Component {
+  constructor() {
+    super();
+    this.funClick = this.funClick.bind(this);
+    this.funClick2 = this.funClick2.bind(this);
+    this.funClick3 = this.funClick3.bind(this);
+  }
+
+    funClick() {
+      console.log('clicou');
+    }
+    
+    funClick2() {
+      console.log('clicou2');
+    }
+    
+    funClick3() {
+      console.log('clicou3');
+    }
+
   render() {
     return (
       <div>
-        <button onClick={funClick}>Clique -me 1</button>
-        <button onClick={funClick2}>Clique -me 2</button>
-        <button onClick={funClick3}>Clique -me 3</button>
+        <button onClick={this.funClick}>Clique -me 1</button>
+        <button onClick={this.funClick2}>Clique -me 2</button>
+        <button onClick={this.funClick3}>Clique -me 3</button>
       </div>
     )
   }

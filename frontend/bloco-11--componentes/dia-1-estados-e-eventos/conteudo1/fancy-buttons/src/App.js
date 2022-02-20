6b2370e3db0fabd74f5dc354453dcbb25765bfac
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-02-20 20:56:35 -0300

    Fixando 5,6 e 7

--- frontend/bloco-11--componentes/dia-1-estados-e-eventos/conteudo1/fancy-buttons/src/App.js
+++ frontend/bloco-11--componentes/dia-1-estados-e-eventos/conteudo1/fancy-buttons/src/App.js
@@ -4,29 +4,45 @@ import React from 'react';
 class Fancy extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      numeroDeCliques: 0,
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+    }
+
     this.funClick = this.funClick.bind(this);
     this.funClick2 = this.funClick2.bind(this);
     this.funClick3 = this.funClick3.bind(this);
   }
 
-    funClick() {
-      console.log('clicou');
-    }
-    
-    funClick2() {
-      console.log('clicou2');
-    }
-    
-    funClick3() {
-      console.log('clicou3');
-    }
+  funClick() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques +1
+    }));
+    console.log('clicou',this);
+  }
+  
+  funClick2() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques1: estadoAnterior.numeroDeCliques1 +1
+    }));
+    console.log('clicou2', this);
+  }
+  
+  funClick3() {
+    this.setState((estadoAnterior, props) => ({
+      numeroDeCliques2: estadoAnterior.numeroDeCliques2 +1
+    }))
+    console.log('clicou3', this);
+  }
 
   render() {
     return (
       <div>
-        <button onClick={this.funClick}>Clique -me 1</button>
-        <button onClick={this.funClick2}>Clique -me 2</button>
-        <button onClick={this.funClick3}>Clique -me 3</button>
+        <button onClick={this.funClick}>Me clicou {this.state.numeroDeCliques} x</button>
+        <button onClick={this.funClick2}>Me clicou {this.state.numeroDeCliques1} x</button>
+        <button onClick={this.funClick3}>Me clicou {this.state.numeroDeCliques2} x</button>
       </div>
     )
   }

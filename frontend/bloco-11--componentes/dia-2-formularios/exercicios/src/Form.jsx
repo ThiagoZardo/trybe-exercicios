65e120516e97c691af186ae7779501e64ad96ec1
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-02-22 19:01:10 -0300

    Adcionei css

--- frontend/bloco-11--componentes/dia-2-formularios/exercicios/src/Form.jsx
+++ frontend/bloco-11--componentes/dia-2-formularios/exercicios/src/Form.jsx
@@ -17,10 +17,19 @@ class Form extends React.Component {
             <input name='radio' type="radio" id="radio" placeholder='UF' />Casa
             <input name='radio' type="radio" id="radio" placeholder='UF' />Apartamento
           </div>
-          <div>
-            <input type="submit" />
-          </div>
         </fieldset>
+
+        <fieldset>
+          <h2>Ultimo Emprego</h2>
+          <textarea placeholder='Resumo do Curriculo...' maxLength="1000" required/>
+          <textarea placeholder='Cargo...' maxLength="40" required />
+          <textarea placeholder='Descrição do Cargo' maxLength="500" required />
+        </fieldset>
+
+        <div className='buttons'>
+            <input type="submit" value="Enviar"/>
+            <input type="submit" value="Limpar" />
+        </div>
       </>
     )
   }
